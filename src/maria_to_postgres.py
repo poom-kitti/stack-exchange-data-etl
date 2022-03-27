972ccc42b2e386fa20463256acad123cf61d7a26
Pattarapon Kittisrisawai <pattarapon.kitti@gmail.com>
2022-03-27 21:21:10 +0700

    format: Move JDBC config to outer scope to conform formatting

--- src/maria_to_postgres.py
+++ src/maria_to_postgres.py
@@ -11,6 +11,20 @@ from .utils.pyspark_utils import JDBCConnectionConfig
 # Configure logger
 logger = _logger.get_default_logger(__name__)
 
+# Initiate JDBC configs
+# Connection to source database is given in
+# https://relational.fit.cvut.cz/dataset/Stats.
+source_db_config = JDBCConnectionConfig.create_mysql_config(
+    hostname="relational.fit.cvut.cz", port="3306", user="guest", password="relational", db_name="stats"
+)
+destination_db_config = JDBCConnectionConfig.create_postgres_config(
+    hostname=os.environ["POSTGRES_HOSTNAME"],
+    port=os.environ["POSTGRES_PORT"],
+    user=os.environ["POSTGRES_USER"],
+    password=os.environ["POSTGRES_PASSWORD"],
+    db_name=os.environ["POSTGRES_DATABASE"],
+)
+
 # Configure spark
 conf = SparkConf()
 conf.set("spark.jars", "jdbc_driver/mysql-connector-java-8.0.28.jar,jdbc_driver/postgresql-42.3.3.jar")
@@ -21,19 +35,6 @@ spark = SparkSession.builder.config(conf=conf).appName("data_preparation").maste
 def main() -> None:
     """Main entry point."""
     logger.info("Initializing task to copy tables in stats database from MariaDB to Postgres.")
-
-    # Connection to source database is given in
-    # https://relational.fit.cvut.cz/dataset/Stats.
-    source_db_config = JDBCConnectionConfig.create_mysql_config(
-        hostname="relational.fit.cvut.cz", port="3306", user="guest", password="relational", db_name="stats"
-    )
-    destination_db_config = JDBCConnectionConfig.create_postgres_config(
-        hostname=os.environ["POSTGRES_HOSTNAME"],
-        port=os.environ["POSTGRES_PORT"],
-        user=os.environ["POSTGRES_USER"],
-        password=os.environ["POSTGRES_PASSWORD"],
-        db_name=os.environ["POSTGRES_DATABASE"],
-    )
 
     for table_name in orig_schema.table_names:
         logger.info("Copying table (%s) from MariaDB to Postgres.", table_name)
